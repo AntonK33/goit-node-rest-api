a47ce50c3371c71ade0d2365901bc0f399ee4a54
AntonK33 <antonkulackov@gmail.com>
2025-01-26 18:52:01 +0100

    r

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -8,7 +8,7 @@ export const createContactSchema = Joi.object({
 export const updateContactSchema = Joi.object({
 
 })
-=======
+
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
