f9e871fb587d44b168c244bce0a84bb37ef6210b
AntonK33 <antonkulackov@gmail.com>
2025-03-04 20:20:50 +0100

    2

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-// app.listen(3000, () => {
-//   console.log("Server is running. Use our API on port: 3000");
-// });
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
+});
 
 const { DB_HOST, PORT = 3000 } = process.env;
 // const DB_HOST = process.env.DB_HOST || 'test'
@@ -48,3 +48,4 @@ mongoose.connect(DB_HOST)
     process.exit(1);
   })
   ;
+  export default app;
