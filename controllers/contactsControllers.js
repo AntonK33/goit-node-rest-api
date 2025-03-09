aeba8246c6e969178e7bbd14baeb8d9c9f6e1104
AntonK33 <antonkulackov@gmail.com>
2025-03-09 18:21:05 +0100

    2

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -43,14 +43,15 @@ export const deleteContact = async (req, res, next) => {
 };
 
 export const createContact = async (req, res, next) => {
-   const { _id: owner } = req.user;
+  const { _id: owner } = req.user;
+  console.log("owner ", owner);
   try {
     const { error } = createContactSchema.validate(req.body);
     if (error) {
       throw HttpError(400, error.message);
     }
   const result = await contactsService.addContact({...req.body, owner});
-
+  console.log("result ", result);
     res.status(201).json(result);
   } catch (error) {
     next(error);
