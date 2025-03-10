2cb42a1e535545cedebe6ccd03a08ab66380b1fc
AntonK33 <antonkulackov@gmail.com>
2025-03-10 16:34:21 +0100

    2

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ import authController from "../controllers/authController.js";
 import validateBody from "../helpers/validateBody.js";
 import { userSignupSchema, userSigninSchema } from "../schemas/usersSchemas.js";
 //import { schemas } from "../models/user.js";
-
+import authenticate from "../middlewares/authenticate.js";
 const route = express.Router();
 
 route.post("/register", validateBody(userSignupSchema), authController.signup);
