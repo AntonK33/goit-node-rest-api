7cecd2b1943ec08b3bbe38af684242ee6565cc99
AntonK33 <antonkulackov@gmail.com>
2025-03-10 21:57:02 +0100

    2

--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const ctrl = require("../../controllers/books");
-const { validateBody, isValidId } = require("../../middlewares");
-const { schemas } = require("../../models/book");
-const router = express.Router();
+// const express = require("express");
+// const ctrl = require("../../controllers/books");
+// const { validateBody, isValidId } = require("../../middlewares");
+// const { schemas } = require("../../models/book");
+// const router = express.Router();
 
-router.get("/", ctrl.getAll);
+// router.get("/", ctrl.getAll);
 
-router.get("/:id", isValidId, ctrl.getById);
+// router.get("/:id", isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas, addSchema), ctrl.add);
+// router.post("/", validateBody(schemas, addSchema), ctrl.add);
 
-router.put("/:id", isValidId, validateBody(schemas, addSchema), ctrl, updateById);
+// router.put("/:id", isValidId, validateBody(schemas, addSchema), ctrl, updateById);
 
-router.patch("/:id/favorite", isValidId, validateBody(schemas, updatrFavoriteSchema));
+// router.patch("/:id/favorite", isValidId, validateBody(schemas, updatrFavoriteSchema));
 
-router.delete("/:id", isValidId, ctrl.deleteById);
+// router.delete("/:id", isValidId, ctrl.deleteById);
 
-module.exports = router;
+// module.exports = router;
